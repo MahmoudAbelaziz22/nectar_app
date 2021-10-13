d5d006675ccd47ee43be98a382a002cbdcb43b3d
Mahmoud Abdelaziz <mahmoud.abdelaziz.mahmoud22@gmail.com>
2021-10-13 12:01:40 +0200

    initial commit

---
+++ lib/app_router.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppRouter {
+  Route? generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      // case HomeScreen.routName:
+      //   return MaterialPageRoute(
+      //       builder: (_) => BlocProvider(
+      //             create: (context) => articlesCubit,
+      //             child: HomeScreen(),
+      //           ));
+
+    }
+  }
+}
---
+++ lib/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class MyColors {
+  static const Color myGrey = Color(0xffF4F4F4);
+  static const Color myGreen = Color(0xff2FBA61);
+}
